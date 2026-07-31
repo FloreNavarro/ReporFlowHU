10f9357e0c40ac21a02afc876f13509dc48f879b
Florencia Navarro <fmnavarro1o11@gmail.com>
2026-07-31 12:06:29 -0300

    agregar ruta para informe medico

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\StudyController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [StudyController::class, 'index'])->name('studies.index');
-
+Route::put('/studies/{id}/report', [StudyController::class, 'updateReport'])->name('studies.updateReport');
 
