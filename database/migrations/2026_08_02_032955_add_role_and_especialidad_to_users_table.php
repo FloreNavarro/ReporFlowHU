a375f62589f6b08a7891dc98386227b8a96b9d9b
Florencia Navarro <fmnavarro1o11@gmail.com>
2026-08-02 00:34:57 -0300

    agregue en base de datos columna especialidad codigo de conexion

--- database/migrations/2026_08_02_032955_add_role_and_especialidad_to_users_table.php
+++ database/migrations/2026_08_02_032955_add_role_and_especialidad_to_users_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('role')->default('Médico')->after('email');
+            $table->string('especialidad')->nullable()->after('role');
         });
     }
 
@@ -22,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn(['role', 'especialidad']);
         });
     }
 };
